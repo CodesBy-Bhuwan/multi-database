64aff83dc3057787f4c9fe2d1aed8710381b7e8c
CodesBy-Bhuwan <CodesByBhuwan@github.com>
2026-05-17 22:35:31 +0545

    Init: sql-sql multiple database setup

---
+++ sql-sql/src/main/java/com/multidb/sql_sql/SqlSqlApplication.java
@@ -0,0 +1,13 @@
+package com.multidb.sql_sql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlSqlApplication.class, args);
+	}
+
+}
---
+++ sql-sql/src/test/java/com/multidb/sql_sql/SqlSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multidb.sql_sql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
